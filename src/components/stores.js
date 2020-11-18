ebfea9bcd2fee3884cbfb557268a20e3f7c71cb0
Xiaojie Si <sixiaojie.jane@gmail.com>
2020-11-18 10:53:21 +0100

    build empty project

--- src/components/stores.js
+++
@@ -1,5 +0,0 @@
-import ThemeStore from './theme-store'
-
-export{
-    ThemeStore
-}
--- src/components/todoContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {TodoListStore, TodoItemStore} from './todoList'
-
-export const TodoListContext = React.createContext({
-    todoListStore: new TodoListStore(),
-    todoItemStore: new TodoItemStore()
-})
-
